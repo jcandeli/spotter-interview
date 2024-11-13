0631a7e0c49168aca3bb6d43e978d884de164d7d
jcandeli <jpcandelier@gmail.com>
2024-11-13 15:30:58 -0800

    Jest working

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,23 +1,23 @@
-// import { formatTime } from "./utils";
+import { formatTime } from "./utils";
 
-// describe("formatTime", () => {
-//   it("formats 0 milliseconds as 00:00", () => {
-//     expect(formatTime(0)).toBe("00:00");
-//   });
+describe("formatTime", () => {
+  it("formats 0 milliseconds as 00:00", () => {
+    expect(formatTime(0)).toBe("00:00");
+  });
 
-//   it("formats 60000 milliseconds (1 minute) as 01:00", () => {
-//     expect(formatTime(60000)).toBe("01:00");
-//   });
+  it("formats 60000 milliseconds (1 minute) as 01:00", () => {
+    expect(formatTime(60000)).toBe("01:00");
+  });
 
-//   it("formats 61000 milliseconds (1 minute 1 second) as 01:01", () => {
-//     expect(formatTime(61000)).toBe("01:01");
-//   });
+  it("formats 61000 milliseconds (1 minute 1 second) as 01:01", () => {
+    expect(formatTime(61000)).toBe("01:01");
+  });
 
-//   it("formats 3661000 milliseconds (61 minutes 1 second) as 61:01", () => {
-//     expect(formatTime(3661000)).toBe("61:01");
-//   });
+  it("formats 3661000 milliseconds (61 minutes 1 second) as 61:01", () => {
+    expect(formatTime(3661000)).toBe("61:01");
+  });
 
-//   it("formats 59000 milliseconds (59 seconds) as 00:59", () => {
-//     expect(formatTime(59000)).toBe("00:59");
-//   });
-// });
+  it("formats 59000 milliseconds (59 seconds) as 00:59", () => {
+    expect(formatTime(59000)).toBe("00:59");
+  });
+});
